bc3db22b92af3c618778a558250dd0a1f99fc1cb
nilayaydin <nilay.aydn97@gmail.com>
2021-12-01 13:15:18 +0300

    Deleted 'git log' and Added 'class Person'

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Hello World')
+class Person {
+    constructor (name,age){
+        this.name = name
+        this.age = age
+        this.photos = []
+        this.likes = []
+    }
+    greet (Person) {
+        console.log (`Hello ${Person.name}, this is ${this.name}`)
+    }
+};
