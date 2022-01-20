080a8826ca048e596d285e35dcef9717767d0d23
niloaydin <nilay.aydn97@gmail.com>
2022-01-20 10:28:22 +0300

    update routes files

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,28 @@
 /* eslint-disable func-names */
 const express = require('express')
+const User = require('../models/User')
+const Order = require('../models/Order')
+const Product = require('../models/Product')
+
+const furkan = new User('furkan', 25)
+const veganBurger = new Product('vegan burger', 'food', 'veggie', 40)
+
+const order1 = new Order()
+order1.addProduct(veganBurger, 2)
+
+furkan.addOrder(order1)
 
 const router = express.Router()
 
 /* GET home page. */
 // eslint-disable-next-line no-unused-vars
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' })
+  res.render('index', { title: 'Express' }) // this index indicates pug file index
+})
+
+// eslint-disable-next-line no-unused-vars
+router.get('/profile', (req, res, next) => {
+  res.render('profile', { user: furkan })
 })
 
 module.exports = router
--- src/routes/users.js
+++ src/routes/users.js
@@ -12,6 +12,7 @@ const neyzen = new User('neyzen', 30)
 const burger = new Product('Vegan burger', 'food', 'veggie', 20)
 const veganPizza = new Product('pizza', 'food', 'aysebrand', 30)
 const order1 = new Order()
+const sinem = new User('Sinem', 18)
 
 // order1.addProduct(burgerOrder)
 order1.addProduct(burger, 2)
@@ -31,11 +32,19 @@ neyzen.likeProduct(burger)
 
 // console.log(order1.item.length)
 
-const users = [neyzen]
+const users = [sinem, neyzen]
+// const start = Date.now()
+// setTimeout(() => {
+//   console.log(`I am Suli, and I am going to come back in ${Date.now() - start} milliseconds.`)
+// })
+
+// for (let i = 0; i < 1000000000; i++) {
+//   Math.random()
+// }
+
 /* GET users listing. */
 router.get('/', (req, res) => {
   let result = users
-
   if (req.query.name) {
     result = users.filter(user => user.name == req.query.name)
   }
@@ -44,10 +53,50 @@ router.get('/', (req, res) => {
 })
 
 router.get('/:userId', (req, res) => {
-  const user = users[req.params.userId]
+  res.send(users[req.params.userId])
 
-  if (user) res.render('user', { user })
-  else res.sendStatus(404)
+  // const user = users[req.params.userId]
+  // if (user) res.render('user', { user })
+  // else res.sendStatus(404)
+})
+
+/* UPDATE users listing. */
+
+router.patch('/:userId', async (req, res) => {
+  const user = await User.findByNameAndUpdate(req.params.name, { name: req.body.name })
+  res.send(user)
+})
+
+/* ADD new users */
+
+router.post('/', async (req, res) => {
+  const { name, age } = req.body
+
+  if (!name || !age) {
+    res
+      .send({
+        message: 'Missing fields.',
+      })
+      .status(400)
+    return
+  }
+
+  // const user = await User.create({
+  //   name,
+  //   age,
+  // })
+  const newUser = new User(name, age)
+  users.push(newUser)
+
+  res.send(newUser)
+})
+
+/* DELETE users */
+
+router.delete('/:id', async (req, res) => {
+  await User.findByIdAndDelete(req.params.id)
+
+  res.sendStatus(200)
 })
 
 module.exports = router
