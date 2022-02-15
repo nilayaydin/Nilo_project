57fa78293d20a5727a1baf4a3b700b92c9247d11
niloaydin <nilay.aydn97@gmail.com>
2022-02-15 17:38:43 +0300

    Add a post handler to order

--- backend/src/routes/orders.js
+++ backend/src/routes/orders.js
@@ -2,6 +2,7 @@ const express = require('express')
 
 const router = express.Router()
 const Order = require('../models/order')
+// const User = require('../models/user')
 
 router.get('/', async (req, res) => {
   const orders = await Order.find()
@@ -10,7 +11,15 @@ router.get('/', async (req, res) => {
 })
 
 router.post('/', async (req, res) => {
-  const createdOrder = await Order.create(req.body)
+  const { orderItems } = req.body
+  // const userId = await User.findById(req.params.userId)
+
+  // eslint-disable-next-line no-underscore-dangle
+  const userId = req.user._id
+  const createdOrder = new Order({ userId, orderItems })
+
+  await createdOrder.calculateAmount()
+
   res.send(createdOrder)
 })
 
