941c6e20c6cf4a328eb699168e434f7378695cd8
niloaydin <nilay.aydn97@gmail.com>
2022-02-16 16:55:20 +0300

    Add a userId to orders.js

--- backend/src/routes/orders.js
+++ backend/src/routes/orders.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const router = express.Router()
 const Order = require('../models/order')
-// const User = require('../models/user')
+const User = require('../models/user')
 
 router.get('/', async (req, res) => {
   const orders = await Order.find()
@@ -12,10 +12,10 @@ router.get('/', async (req, res) => {
 
 router.post('/', async (req, res) => {
   const { orderItems } = req.body
-  // const userId = await User.findById(req.params.userId)
+  const userId = await User.findById(req.params.userId)
 
   // eslint-disable-next-line no-underscore-dangle
-  const userId = req.user._id
+  // const userId = req.user._id
   const createdOrder = new Order({ userId, orderItems })
 
   await createdOrder.calculateAmount()
