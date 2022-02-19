11f95e54ad9a65493e2184193d89148fea33c9f7
niloaydin <nilay.aydn97@gmail.com>
2022-02-20 01:24:40 +0300

    Changed the order model and update users route accordingly

--- backend/src/models/order.js
+++ backend/src/models/order.js
@@ -5,50 +5,62 @@ const autopopulate = require('mongoose-autopopulate')
 const orderSchema = new mongoose.Schema({
   orderItems: [
     {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Product',
-      required: true,
-      autopopulate: { maxDepth: 1 },
+      item: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product',
+        required: true,
+        autopopulate: { maxDepth: 5 },
+      },
+      quantity: {
+        type: Number,
+        default: 1,
+        required: true,
+      },
     },
   ],
-  quantity: [],
-  amount: 0,
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    autopopulate: true,
+    required: true,
+  },
+  amount: {
+    type: Number,
+    default: 0,
+  },
 })
 class Order {
   async addProduct(product, quantity = 1) {
-    // TODO: No need to use some. Refactor the code.
-    // eslint-disable-next-line no-console
-    console.log(product)
-    if (this.orderItems.some(orderItem => orderItem.id == product.id)) {
-      this.orderItems.find(orderItem => orderItem.id == product.id).quantity += quantity
-      return
+    const currentItem = this.orderItems.find(orderItem => orderItem.id == product.id)
+    if (currentItem) {
+      currentItem.quantity += quantity
+    } else {
+      this.orderItems.push({ item: product, quantity })
     }
 
-    this.orderItems.push(product)
-    this.quantity.push(quantity)
     await this.save()
   }
 
   async calculateAmount() {
     for (let i = 0; i < this.orderItems.length; i += 1) {
-      this.amount += this.orderItems[i].price * this.quantity[i]
+      this.amount += this.orderItems[i].item.price * this.orderItems[i].quantity
     }
     await this.save()
   }
-
-  // async calculateAmount() {
-  //   // eslint-disable-next-line no-const-assign
-  //   for (let i = 0; i < this.orderItems.length; i++) {
-  //     this.amount += this.orderItems[i].amount
-  //   }
-  //   return this.amount
-  // }
-
-  // addProduct(productId, quantity) {
-  //   const firstOrderItem = new Orderitem(this.orderId, productId, quantity)
-  //   this.item.push(firstOrderItem)
-  // }
 }
+
+// async calculateAmount() {
+//   // eslint-disable-next-line no-const-assign
+//   for (let i = 0; i < this.orderItems.length; i++) {
+//     this.amount += this.orderItems[i].amount
+//   }
+//   return this.amount
+// }
+
+// addProduct(productId, quantity) {
+//   const firstOrderItem = new Orderitem(this.orderId, productId, quantity)
+//   this.item.push(firstOrderItem)
+// }
 
 orderSchema.loadClass(Order)
 orderSchema.plugin(autopopulate)
