dd958c44a29f119afdf43e235e265fd6028ea1f4
niloaydin <nilay.aydn97@gmail.com>
2022-03-01 04:34:54 +0300

    Create product-card and product-list

---
+++ frontend/src/components/product-card.vue
@@ -0,0 +1,37 @@
+<script>
+import { mapActions } from 'vuex'
+
+export default {
+  name: 'ProductCard',
+  props: ['product'],
+  methods: {
+    ...mapActions(['createOrder']),
+  },
+}
+</script>
+
+<template lang="pug">
+  .product-card
+    h4 {{ product.name }}
+    p {{ product.brand }}
+    p Price: {{ product.price }}₺
+    p Category: {{ product.category }}
+    img(:src="require(`@/assets/${product.photo}`)")
+    button(@click="createOrder") Add Product
+
+</template>
+
+<style lang="scss">
+.product-card {
+  border: 1px solid #0f0f0f;
+  border-radius: 8px;
+  padding: 8px;
+  margin-bottom: 12px;
+  min-height: 220px;
+
+  img {
+    width: 100px;
+    height: 100px;
+  }
+}
+</style>
---
+++ frontend/src/views/product-list.vue
@@ -0,0 +1,48 @@
+<script>
+// import Counter from '@/components/counter.vue'
+import ProductCard from '@/components/product-card.vue'
+import { mapActions } from 'vuex'
+
+export default {
+  name: 'ProductList',
+  components: { ProductCard },
+  data() {
+    return {
+      products: [],
+    }
+  },
+  async created() {
+    this.products = await this.fetchProducts()
+  },
+  methods: {
+    ...mapActions(['fetchProducts']),
+  },
+}
+</script>
+
+<template>
+  <div id="app">
+    <div class="header">
+      <h1>Products</h1>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div v-for="product of products" :key="product" class="col-12 col-md-6 col-lg-4">
+          <ProductCard :product="product" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+@import '@/assets/theme.scss';
+@import 'bootstrap/scss/bootstrap.scss';
+
+.header {
+  padding: 60px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
