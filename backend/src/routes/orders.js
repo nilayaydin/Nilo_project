dced044dcd8171014cb0a97c52220ea048ac3017
niloaydin <nilay.aydn97@gmail.com>
2022-03-02 19:06:19 +0300

    Fix bugs in post req

--- backend/src/routes/orders.js
+++ backend/src/routes/orders.js
@@ -6,24 +6,32 @@ const User = require('../models/user')
 // const Product = require('../models/product')
 
 router.get('/', async (req, res) => {
-  const orders = await Order.find({}) // change the line 10th to this
+  const orders = await Order.find() // change the line 10th to this
   // const orders = await Order.find({})
 
   res.send(orders)
 })
 
+router.get('/:orderId', async (req, res) => {
+  const order = await Order.findById(req.params.orderId)
+
+  if (order) res.send(order)
+  else res.sendStatus(404)
+})
+
 // eslint-disable-next-line consistent-return
 router.post('/', async (req, res) => {
-  const { orderItems, userId } = req.body
+  const { orderItems } = req.body
 
-  if (!userId) {
+  if (!req.user) {
     return res.send(403)
   }
 
   // eslint-disable-next-line no-console
   console.log('looking for orderItems', orderItems)
 
-  // const userId = req.user._id
+  const userId = req.user._id
+
   // eslint-disable-next-line no-console
   console.log('trying to find userId', userId)
 
@@ -33,11 +41,11 @@ router.post('/', async (req, res) => {
 
   // eslint-disable-next-line no-underscore-dangle
   const order = await Order.findById(createdOrder._id)
-  const user = await User.findById(userId)
+  // const user = await User.findById(userId)
 
   await order.calculateAmount()
-  // await req.user.addOrder(order)
-  await user.addOrder(order)
+  await req.user.addOrder(order)
+  //  await user.addOrder(order)
 
   res.send(order)
 })
