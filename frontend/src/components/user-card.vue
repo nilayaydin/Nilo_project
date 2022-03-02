d92be11104e256725f6107a1286ba9ab73590285
niloaydin <nilay.aydn97@gmail.com>
2022-03-02 19:07:08 +0300

    Add adding address function, quantity and amount

--- frontend/src/components/user-card.vue
+++ frontend/src/components/user-card.vue
@@ -1,9 +1,22 @@
 <script>
 // Is this working?
+import { mapActions } from 'vuex'
 
 export default {
   name: 'UserCard',
   props: ['user'],
+  data() {
+    return {
+      address: null,
+    }
+  },
+  methods: {
+    ...mapActions(['addAddress']),
+    async createAddress() {
+      await this.addAddress({ address: this.address })
+      alert('Address is successfully added!')
+    },
+  },
 }
 </script>
 
@@ -17,9 +30,15 @@ export default {
   .order-info(v-for="order in user.orders")
     ul(v-for="orderItem in order.orderItems")
       li {{ orderItem.item.name }}
+       h6 Quantity: {{ orderItem.quantity }}
+       h6 Total Amount: {{ order.amount }}
   .liked-products(v-for="product in user.likesProduct")
     h4 Liked Products:
     li {{ product.name }} ( {{ product.brand }} )
+  .addAddress
+  label(for="addresses") Create Address
+    input(v-model="address", id="address", type="text", placeholder="type here")
+    button(@click="createAddress") Submit
 
 </template>
 
